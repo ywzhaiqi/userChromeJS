6a3fad38b07d7986fdd8ea52c203f558986725dc
ywzhaiqi <ywzhaiqi@gmail.com>
2013-06-09 01:51:23 +0800

    默认禁用了 frame 下的下一页

--- Space_nextpage/space_nextpage.uc.js
+++ Space_nextpage/space_nextpage.uc.js
@@ -5,40 +5,43 @@
 // ==/UserScript==
 
 (function() {
-
-    var space = ' '.charCodeAt(0);
+    var space = " ".charCodeAt(0);
     gBrowser.mPanelContainer.addEventListener('keypress', function(event) {
-        if (event.charCode != space) return;
-        if (event.target.nodeName.match(/input|textarea|select/i)) return;
+        if (event.charCode != space){
+            return;
+        }
 
         // 有小说阅读脚本的逃过
         if(content.wrappedJSObject.readx){
             return;
         }
 
-        var doc = content.document,
-            win = content.window;
+        if (event.target.nodeName.match(/input|textarea|select/i)){
+            return;
+        }
 
-        var clientHeight = doc.body.clientHeight;
-        var scrollTop = doc.body.scrollTop | doc.documentElement.scrollTop;
-        var windowHeight = doc.all ? doc.getElementsByTagName("html")[0].offsetHeight : win.innerHeight;
-        // 为什么 Firefox 下要加1？
-        if ((scrollTop + windowHeight + 1) >= clientHeight) {
-            // alert("你到页面底部了");
-            // super_preloader_next();
-            // nextPage.next(true, null , "lastLink");
+
+        var node = event.target;
+        while (node && node.nodeName != "BODY")
+            node = node.parentNode; 
+
+        var 
+            doc = node.ownerDocument,
+            win = doc.defaultView,
+            isFrame = (win.top != win)
+        ;
+
+        if(isFrame){
+            return;
+        }
+
+        if(content.scrollMaxY <= content.scrollY){
             nextPage.next(true);
         }
 
     }, false);
 
-    
-
-    // function super_preloader_next() {
-    //     var document = (this.content && this.content.window) ? this.content.window.wrappedJSObject.document : this.document;
-    //     var event = document.createEvent('HTMLEvents');
-    //     event.initEvent('superPreloader.go', true, false);
-    //     document.dispatchEvent(event);
-    // }
-    
+    function debug(){
+        content.console.log.apply(this, arguments);
+    }
 })();

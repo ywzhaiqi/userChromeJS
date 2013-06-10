171bdb8ef30e5a0e3e84c061276047dc2dbfa901
ywzhaiqi <ywzhaiqi@gmail.com>
2013-06-10 17:55:18 +0800

    完善iframe里下一页的支持

--- Space_nextpage/space_nextpage.uc.js
+++ Space_nextpage/space_nextpage.uc.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name          空格键页面底部翻页（调用nextPage.uc.xul）
+// @description   默认设置小说阅读脚本启用后禁用。
 // @author        ywzhaiqi@gmail.com
 // @include       main
 // ==/UserScript==
 
 (function() {
     var space = " ".charCodeAt(0);
+
     gBrowser.mPanelContainer.addEventListener('keypress', function(event) {
         if (event.charCode != space){
             return;
@@ -26,21 +28,34 @@
         ;
 
         // 有小说阅读脚本的逃过
-        if(content.wrappedJSObject.readx && doc.body.getAttribute("name") == "MyNovelReader"){
+        // content.wrappedJSObject.readx && 
+        if(doc.body.getAttribute("name") == "MyNovelReader"){
             return;
         }
 
         if(isFrame){
-
+            var clientHeight = doc.body.clientHeight;
+            var scrollTop = doc.body.scrollTop | doc.documentElement.scrollTop;
+            var windowHeight = doc.all ? doc.getElementsByTagName("html")[0].offsetHeight : win.innerHeight;
+            // 为什么 Firefox 下要加1？
+            if ((scrollTop + windowHeight + 1) >= clientHeight) {
+                goToNextPage();
+            }
         }else{
             if(content.scrollMaxY <= content.scrollY){
-                nextPage.next(true);
+                goToNextPage();
             }
         }
 
     }, false);
 
+    function goToNextPage(){
+        nextPage.next(true);
+        // alert("goToNextPage")
+    }
+
     function debug(arg){
         content.console.log("[space_nextpage] " + arg);
     }
+
 })();

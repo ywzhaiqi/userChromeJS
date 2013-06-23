f33c26277616a3f8e2c28b5c42b57eea6ed58883
ywzhaiqi <ywzhaiqi@gmail.com>
2013-06-23 22:15:16 +0800

    init

---
+++ chromejs.uc.js
@@ -0,0 +1,9 @@
+// 在地址栏打开 chromejs
+location == "chrome://browser/content/browser.xul" && (function(){
+	var urlbar = document.getElementById("urlbar");
+	try{
+		eval("urlbar.handleCommand="+ urlbar.handleCommand.toString(
+		    ).replace("var url = this.value;", "var url = this.value;" + 
+		    "if(url.indexOf('chromejs') == 0) return eval(url.slice(9));"));
+	}catch(e) {}
+})();

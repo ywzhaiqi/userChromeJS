138c21527912b9ccd66dcb26628447393ce566ed
ywzhaiqi <ywzhaiqi@gmail.com>
2013-08-06 17:10:21 +0800

    space_nextpage.uc.js 重新调整结构,增加 uAutoPagerize、uSuper_preloader 的支持

--- Space_nextpage/space_nextpage.uc.js
+++ Space_nextpage/space_nextpage.uc.js
@@ -1,61 +1,77 @@
 // ==UserScript==
-// @name          空格键页面底部翻页（调用nextPage.uc.xul）
-// @description   默认设置小说阅读脚本启用后禁用。
+// @name          空格键页面底部翻页（依次调用 uAutoPagerize、uSuper_preloader、nextPage.uc.xul）
+// @description   默认设置小说阅读脚本启用后禁用
 // @author        ywzhaiqi@gmail.com
 // @include       main
 // ==/UserScript==
 
-(function() {
-    var space = " ".charCodeAt(0);
+if(window.Space_nextpage){
+    window.Space_nextpage.uninit();
+    window.Space_nextpage = null;
+}
 
-    gBrowser.mPanelContainer.addEventListener('keypress', function(event) {
-        if (event.charCode != space){
-            return;
+window.Space_nextpage = {
+    init: function(){
+        gBrowser.mPanelContainer.addEventListener('keypress', this, false);
+    },
+    uninit: function(){
+        gBrowser.mPanelContainer.removeEventListener('keypress', this, false);
+    },
+    handleEvent: function(event){
+        switch(event.type){
+            case "keypress":
+                this.keypress(event);
+                break;
         }
-
-        if (event.target.nodeName.match(/input|textarea|select/i)){
-            return;
+    },
+    goToNextPage: function(win){
+        if (win.ap) {
+            let nextURL = win.ap.nextLink && win.ap.nextLink.href;
+            if (nextURL)
+                win.location = nextURL;
+        } else if (win.uSuper_preloader) {
+            win.uSuper_preloader.go();
+        } else if (window.nextPage) {
+            nextPage.next(true);
         }
+    },
+    space: " ".charCodeAt(0),
+    keypress: function(event){
+        if (event.charCode != this.space)
+            return;
 
-        var node = event.target;
-        while (node && node.nodeName != "BODY")
-            node = node.parentNode; 
+        if (event.target.nodeName.match(/input|textarea|select/i))
+            return;
 
-        var 
-            doc = node.ownerDocument,
+        var
+            doc = event.target.ownerDocument,
             win = doc.defaultView,
             isFrame = (win.top != win)
         ;
 
-        // 有小说阅读脚本的逃过
-        // content.wrappedJSObject.readx && 
-        if(doc.body.getAttribute("name") == "MyNovelReader"){
+        if(!doc.body)
             return;
-        }
+
+        // 鲜果阅读器防止出错
+        if(win.location.href.match(/xianguo\.com\/reader/i))
+            return;
+
+        // 有小说阅读脚本的逃过
+        if(doc.body.getAttribute("name") == "MyNovelReader")
+            return;
 
         if(isFrame){
             var clientHeight = doc.body.clientHeight;
             var scrollTop = doc.body.scrollTop | doc.documentElement.scrollTop;
             var windowHeight = doc.all ? doc.getElementsByTagName("html")[0].offsetHeight : win.innerHeight;
             // 为什么 Firefox 下要加1？
-            if ((scrollTop + windowHeight + 1) >= clientHeight) {
-                goToNextPage();
-            }
+            if ((scrollTop + windowHeight + 1) >= clientHeight)
+                this.goToNextPage(win);
         }else{
-            if(content.scrollMaxY <= content.scrollY){
-                goToNextPage();
-            }
+            if(content.scrollMaxY <= content.scrollY)
+                this.goToNextPage(win);
         }
-
-    }, false);
-
-    function goToNextPage(){
-        nextPage.next(true);
-        // alert("goToNextPage")
     }
+};
 
-    function debug(arg){
-        content.console.log("[space_nextpage] " + arg);
-    }
-
-})();
+window.Space_nextpage.init();

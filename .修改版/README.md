bc478eb4fea7d4d23132cb0221a2e636b86f4092
ywzhaiqi <ywzhaiqi@gmail.com>
2014-07-12 14:42:42 +0800

    新增：2个更新播放器的 bat

--- .修改版/README.md
+++ .修改版/README.md
@@ -24,6 +24,7 @@
     - 添加了一行 `&& doc.contentType != 'text/javascript'` 增加 js 文件的支持。
  - [youkuantiadsModY.uc.js](youkuantiadsModY.uc.js)，[youkuantiads 视频去广告去黑屏](http://bbs.kafan.cn/thread-1509944-1-1.html) 修改版。
     - 新增：本地播放器检测功能。如果有本地播放器则使用本地的路径，否则使用默认的网络播放器。
-    - 新增：提前判断是否为 flash，加快速度。
+    - ~~新增：提前判断是否为 flash，加快速度。~~ *已反馈给作者，作者的7月2日版本已经增加。*
     - 本地播放器默认路径为 chrome 目录下的 swf 文件夹。
+    - swf 下的是 bat 更新文件，需要额外的 [wget.exe](http://users.ugent.be/~bpuype/wget/)
     - 本地的 17173 播放器默认未启用。
---
+++ .修改版/swf/update.bat
@@ -0,0 +1,39 @@
+@echo off 
+cd /d %~dp0
+::更新视频播放器
+REM set dd=wget.exe -N -P ..\swf\
+set dd=wget.exe -N -P ..\swf\
+
+::youku
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/player.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/loader.swf
+
+::ku6
+%dd% http://haoutil.googlecode.com/svn/trunk/player/ku6.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/ku6_out.swf
+
+::iqiyi
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi5.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi_out.swf
+
+::tudou
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/tudou.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/sp.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/olc_8.swf
+
+::letv
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/letv.swf
+%dd% http://player.letvcdn.com/p/201403/05/1456/newplayer/1/SLetvPlayer.swf
+
+::pplive
+%dd% http://haoutil.googlecode.com/svn/trunk/player/pplive.swf
+%dd% http://haoutil.googlecode.com/svn/trunk/player/pplive_live.swf
+
+::Sohu
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/sohu.swf
+
+::pps
+%dd% http://haoutil.googlecode.com/svn/trunk/player/testmod/pps.swf
+
+::17173
